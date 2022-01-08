a9e358f7a9dfcb4d25f812b61110d13f8ef0b6f6
Hillary-Kechem <hillarykechem92@gmail.com>
2022-01-08 10:35:07 +0300

    toogle between what we do

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("#design").click(function () {
+      $("#design2").toggle();
+      $("#hidden").hide();
+    });
+})
+$(document).ready(function () {
+    $("#design").click(function () {
+      $("#infor").toggle();
+      $("#hidden").toggle();
+    });
+})
+$(document).ready(function () {
+    $("#design").click(function () {
+      $("#infor").toggle();
+      $("#hidden").toggle();
+    });
+})
