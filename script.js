b35a45d0bb79181c7ae80fa20b3ceffcacae8287
Hillary-Kechem <hillarykechem92@gmail.com>
2022-01-08 15:29:48 +0300

    portfolio fade

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
 $(document).ready(function () {
-    $("#design").click(function () {
-      $("#design2").toggle();
-      $("#hidden").hide();
+    $("#development").click(function () {
+    $("#development2").toggle();
+    
     });
 })
-$(document).ready(function () {
-    $("#design").click(function () {
-      $("#infor").toggle();
-      $("#hidden").toggle();
-    });
-})
-$(document).ready(function () {
-    $("#design").click(function () {
-      $("#infor").toggle();
-      $("#hidden").toggle();
-    });
-})
+
+$("#blackimg").hover(
+    function () {
+      $(this).find(".title").fadeIn(1000);
+    },
+  );
+  $("#ontario").hover(
+    function () {
+      $(this).find(".title2").fadeIn(1000).fadeOut(2000);
+    },
+  );
+  $("#yellowimg").hover(
+    function () {
+      $(this).find(".title3").fadeIn(1000).fadeOut(2000);
+    },
+  );
+  $("#disney").hover(
+    function () {
+      $(this).find(".title4").fadeIn(1000).fadeOut(2000);
+    },
+  );
+  $("#good").hover(
+    function () {
+      $(this).find(".title5").fadeIn(1000).fadeOut(2000);
+    },
+  );
+  $("#button").hover(
+    function () {
+      $(this).find(".title6").fadeIn(1000).fadeOut(2000);
+    },
+  );
+  $("#burnedlogo").hover(
+    function () {
+      $(this).find(".title7").fadeIn(1000).fadeOut(2000);
+    },
+  );
+  $("#bailey").hover(
+    function () {
+      $(this).find(".title8").fadeIn(1000).fadeOut(2000);
+    },
+  );
+  
+  
