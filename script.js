60b8982b4d569b5c12d1b481b11af85cb90d06ca
Hillary-Kechem <hillarykechem92@gmail.com>
2022-01-09 20:58:02 +0300

    script final

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ $(document).ready(function () {
     $("#infor").click(function () {
     $("#hidden").toggle();
     
-    });
+ });
     $("#development").click(function () {
-        $("#development2").toggle();
-        
-        });
-        $("#product1").click(function () {
-            $("#hidden2").toggle();
-            
-            });
+    $("#hidden1").toggle();       
+ });
+    $("#product1").click(function () {
+    $("#hidden2").toggle();
+     });
 })
 
 $("#blackimg").hover(
@@ -53,5 +51,39 @@ $("#blackimg").hover(
       $(this).find(".title8").fadeIn(1000).fadeOut(2000);
     },
   );
+
+
+  var validation = function () {
+    var name = document.getElementById("inputName").value;
+    var email = document.getElementById("inputEmail").value;
+    var message = document.getElementById("inputMessage").value;
+
+
+    $("form").on("submit", function (e) {
+        e.preventDefault();
+        var name = $("#inputName").val();
+        alert(
+          "Hello" + " " + name + ",\nWe have received your message. " + "\nThank you for contacting us at Delani Studio."
+        );
+        document.querySelector("form").reset();
+      });
+
+    if (name == " ") {
+        alert("Please enter a valid name! \nName must be filled")
+        return false;
+    };
+
+       if (email == " ") {
+        alert("Email must be filled!")
+        return false;
+    };
+
+    if (message == " ") {
+        alert("You have not entered any message, or your message is too short.")
+        return false;
+    } else {
+        alert("Hi " + name + ", your message was successfully sent. Thank you for reaching out to us.")
+    }
+};
   
   
